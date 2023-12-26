883e5bf190bcdc8622ac8186b4cce511c242234f
Fajar Julyana <fajarjulyana@github.com>
2023-12-26 17:40:55 +0700

    background.png build CMakeLists.txt LICENSE main.c README.mdadded

---
+++ build/CMakeFiles/HelloGTK.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloGTK.dir/main.c.o"
+  "CMakeFiles/HelloGTK.dir/main.c.o.d"
+  "HelloGTK"
+  "HelloGTK.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HelloGTK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
